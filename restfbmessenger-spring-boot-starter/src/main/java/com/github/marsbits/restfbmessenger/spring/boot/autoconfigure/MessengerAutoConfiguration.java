16f880cb6eaabaf374f73dd56b947274fe76d71e
azimka <aszimka@gmail.com>
2017-11-27 11:01:14 +0300

    GET_STARTED test research

--- restfbmessenger-spring-boot-starter/src/main/java/com/github/marsbits/restfbmessenger/spring/boot/autoconfigure/MessengerAutoConfiguration.java
+++ restfbmessenger-spring-boot-starter/src/main/java/com/github/marsbits/restfbmessenger/spring/boot/autoconfigure/MessengerAutoConfiguration.java
@@ -76,7 +76,7 @@ public class MessengerAutoConfiguration {
             String path = properties.getWebhook().getPath();
             String urlMapping = (path.endsWith("/") ? path + "*" : path + "/*");
             WebhookServlet servlet = new WebhookServlet(messenger);
-            messenger.setGetStartedButton("test123");
+            messenger.setGetStartedButton("GET_STARTED_PAYLOAD");
             return new ServletRegistrationBean(servlet, urlMapping);
         }
     }
